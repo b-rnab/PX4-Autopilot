056862b6e1c1fecec40329b33315044936b8eeea
Kate <kdiomkina@ukr.net>
2026-06-04 15:09:16 +0200

    started on new module

---
+++ src/modules/spoofing_detector/SpoofingDetector.cpp
@@ -0,0 +1,177 @@
+#include "SpoofingDetector.hpp"
+
+//connects the module to commands
+ModuleBase::Descriptor SpoofingDetector::desc{
+	SpoofingDetector::task_spawn,
+	SpoofingDetector::custom_command,
+	SpoofingDetector::print_usage
+};
+
+//initializes the ScheduledWorkItem
+SpoofingDetector::SpoofingDetector() :
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::lp_default)
+{
+}
+
+int SpoofingDetector::task_spawn(int argc, char *argv[])
+{
+	SpoofingDetector *obj = new SpoofingDetector();
+
+	if (!obj) {
+		PX4_ERR("alloc failed");
+		return -1;
+	}
+
+	desc.object.store(obj);
+	desc.task_id = task_id_is_work_queue;
+
+	if (!obj->start()){
+		delete obj;
+		desc.object.store(nullptr);
+		desc.task_id = -1;
+		return -1;
+	}
+
+	return 0;
+}
+
+int SpoofingDetector::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int SpoofingDetector::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+Carrier phase and IMU based GNSS spoofing detector.
+
+The module subscribes to sensor_gps_raw and vehicle_imu. It collects windowed
+measurements and can print a warning when spoofing is detected.
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("spoofing_detector", "system");
+	PRINT_MODULE_USAGE_COMMAND_DESCR("start", "Start the spoofing detector");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+int SpoofingDetector::print_status()
+{
+	// print status
+	return 0;
+}
+
+bool SpoofingDetector::start()
+{
+	if (!_sensor_gps_raw_sub.registerCallback()) {
+		PX4_ERR("callback registration failed");
+		return false;
+	}
+
+	return true;
+}
+
+void SpoofingDetector::addGpsSample(const sensor_gps_raw_s &gps_raw)
+{
+	if (_gps_buffer_count < GPS_BUFFER_SIZE) {
+		_gps_buffer_count++;
+	} else {
+		for (int i = 1; i < GPS_BUFFER_SIZE; i++) {
+			_gps_buffer[i - 1] = _gps_buffer[i];
+		}
+	}
+
+	const int index = _gps_buffer_count - 1;
+
+	_gps_buffer[index].timestamp_sample = gps_raw.timestamp_sample;
+
+	for (int i = 0; i < 32; i++){
+		_gps_buffer[index].carrier_phase[i] = gps_raw.carrier_phase[i];
+		_gps_buffer[index].flags[i] = gps_raw.flags[i];
+
+		//every time a new carrier phase value is added its detrended value should be set to zero
+		_gps_buffer[index].detrended_carrier_phase[i] = 0.0;
+		_gps_buffer[index].detrended_valid[i] = false;
+	}
+
+}
+
+void SpoofingDetector::addImuSample(const vehicle_imu_s &imu)
+{
+	if (_imu_buffer_count < IMU_BUFFER_SIZE) {
+		_imu_buffer_count++;
+	} else {
+		for (int i = 1; i < IMU_BUFFER_SIZE; i++) {
+			_imu_buffer[i - 1] = _imu_buffer[i];
+		}
+	}
+
+	const int index = _imu_buffer_count - 1;
+
+	_imu_buffer[index].timestamp_sample = imu.timestamp_sample;
+	_imu_buffer[index].delta_velocity_dt = imu.delta_velocity_dt;
+
+	for (int i = 0; i < 3; i++) {
+		_imu_buffer[index].delta_velocity[i] = imu.delta_velocity[i];
+	}
+}
+
+bool SpoofingDetector::windowReady()
+{
+	return _gps_buffer_count >= GPS_BUFFER_SIZE && _imu_buffer_count > 0;
+}
+
+void SpoofingDetector::detrendSample()
+{
+	const int index = _gps_buffer_count - 1;
+}
+
+bool SpoofingDetector::runSpoofingDetection()
+{
+	return false;
+}
+
+void SpoofingDetector::Run()
+{
+	if (should_exit()) {
+		_sensor_gps_raw_sub.unregisterCallback();
+		ScheduleClear();
+		exit_and_cleanup(desc);
+		return;
+	}
+
+	vehicle_imu_s imu;
+
+	while (_vehicle_imu_sub.update(&imu)) {
+		addImuSample(imu);
+	}
+
+	sensor_gps_raw_s gps_raw;
+
+	while (_sensor_gps_raw_sub.update(&gps_raw)) {
+		addGpsSample(gps_raw);
+
+		if (windowReady()) {
+			detrendSample();
+			bool spoofed = runSpoofingDetection();
+
+			if (spoofed && !_spoofing_detected_prev) {
+				PX4_WARN("GNSS spoofing detected");
+			}
+
+			_spoofing_detected_prev = spoofed;
+		}
+	}
+}
+
+extern "C" __EXPORT int spoofing_detector_main(int argc, char *argv[])
+{
+	return ModuleBase::main(SpoofingDetector::desc, argc, argv);
+}
---
+++ src/modules/spoofing_detector/SpoofingDetector.hpp
@@ -0,0 +1,75 @@
+//prevents the header from beign included twice
+#pragma once
+
+//gives the module support like start, stop, status, ModulBase, Descriptor etc
+#include <px4_platform_common/module.h>
+//Lets the module run in the work queue
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+//Let the module read uORB topics
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+//the message types that will be used in the module
+#include <uORB/topics/sensor_gps_raw.h>
+#include <uORB/topics/vehicle_imu.h>
+
+//the spoofind detector is a module and an item in the work queue
+class SpoofingDetector : public ModuleBase, public px4::ScheduledWorkItem
+{
+public:
+	//bookkeeping object for the module
+	static Descriptor desc;
+
+	//constructor and destructor
+	SpoofingDetector();
+	~SpoofingDetector() override = default;
+
+	//commands in the shell
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+
+	int print_status() override;
+
+private:
+	//the main function
+	void Run() override;
+
+	//functions for spoofing detection
+	void addGpsSample(const sensor_gps_raw_s &gps_raw);
+	void addImuSample(const vehicle_imu_s &imu);
+	void detrendSample();
+	bool windowReady();
+	bool runSpoofingDetection();
+	bool start();
+
+	//buffer structs
+	struct GpsSample {
+		uint64_t timestamp_sample{};
+		double carrier_phase[32]{};
+		double detrended_carrier_phase[32]{};
+		uint8_t flags[32]{};
+		bool detrended_valid[32]{};
+	};
+
+	struct ImuSample {
+		uint64_t timestamp_sample{};
+		float delta_velocity[3]{};
+		uint32_t delta_velocity_dt{};
+	};
+
+	//window buffers
+	static constexpr int GPS_BUFFER_SIZE = 20;
+	static constexpr int IMU_BUFFER_SIZE = 1000;
+	GpsSample _gps_buffer[GPS_BUFFER_SIZE]{};
+	ImuSample _imu_buffer[IMU_BUFFER_SIZE]{};
+
+	int _gps_buffer_count{0};
+	int _imu_buffer_count{0};
+
+	bool _spoofing_detected_prev{false};
+
+	//subscribe to sensor_gps_raw and vehicle_imu and use sensor_gps_raw a triger
+	uORB::SubscriptionCallbackWorkItem _sensor_gps_raw_sub{this, ORB_ID(sensor_gps_raw)};
+	uORB::Subscription _vehicle_imu_sub{ORB_ID(vehicle_imu)};
+};
